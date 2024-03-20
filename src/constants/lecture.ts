9b39cdaa3dd5d38e1dc00f79c43aae1fd620f65e
DGUHJH <ghkdwogud852@gmail.com>
2024-03-20 22:28:35 +0900

    chore : lecture constant 추가

---
+++ src/constants/lecture.ts
@@ -0,0 +1,13 @@
+export const availableYears = [2020, 2021, 2022, 2023, 2024]
+export type AvailableYearType = 2020 | 2021 | 2022 | 2023 | 2024
+export type AvailableSeasonType = '1' | 'summer' | '2' | 'winter'
+
+export const availableSeason = (year: AvailableYearType): AvailableSeasonType[] => {
+  if (year === 2020 || year === 2021 || year === 2022 || year === 2023) {
+    return ['1', 'summer', '2', 'winter']
+  }
+  if (year === 2024) {
+    return ['1']
+  }
+  return []
+}
