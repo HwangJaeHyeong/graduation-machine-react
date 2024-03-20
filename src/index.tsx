656e38a01c47314681b4ac23c00042b65e568cd5
DGUHJH <ghkdwogud852@gmail.com>
2024-03-20 20:41:37 +0900

    initialize

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { MainPage } from 'pages/Main'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as any)
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
