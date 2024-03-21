f81ba51e130a7d69e939e0bef93ccfa1a5d8b9b1
DGUHJH <ghkdwogud852@gmail.com>
2024-03-21 14:57:48 +0900

    chore : xlsx 모듈 추가

--- src/pages/Lecture/Excel/LectureExcelPage.tsx
+++ src/pages/Lecture/Excel/LectureExcelPage.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react'
-import { Root } from './styled'
+
+import { ContentContainer, HeaderContainer, HeaderLogoTypo, Root } from './styled'
 
 type LectureExcelPageProps = {
   className?: string
 }
 
 export const LectureExcelPage: FC<LectureExcelPageProps> = ({ className }) => {
-  return <Root className={className}>LectureExcelPage</Root>
+  return (
+    <Root className={className}>
+      <HeaderContainer>
+        <HeaderLogoTypo>종합 시간표 엑셀 입력하기</HeaderLogoTypo>
+      </HeaderContainer>
+      <ContentContainer>asd</ContentContainer>
+    </Root>
+  )
 }
--- src/pages/Lecture/Excel/styled.ts
+++ src/pages/Lecture/Excel/styled.ts
@@ -1,3 +1,53 @@
+import { Typography } from 'antd'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  padding: 80px 0;
+`
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  border-bottom: 1px #c9c9c9 solid;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02), 0 2px 4px 0 rgba(0, 0, 0, 0.02);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`
+
+export const HeaderLogoTypo = styled(Typography)`
+  &&& {
+    font-size: 32px;
+    font-weight: 700;
+  }
+`
+
+export const HeaderMenuContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`
+
+export const ContentContainer = styled.div`
+  width: 1000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 30px 20px;
+  box-sizing: border-box;
+  border: 1px #c9c9c9 solid;
+  border-radius: 8px;
+  margin-top: 20px;
+`
