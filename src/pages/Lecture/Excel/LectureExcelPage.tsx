e78af0811e82f582d7d2e8efa65ffd8502542431
DGUHJH <ghkdwogud852@gmail.com>
2024-03-21 18:00:12 +0900

    feat : initialize excel filter

--- src/pages/Lecture/Excel/LectureExcelPage.tsx
+++ src/pages/Lecture/Excel/LectureExcelPage.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react'
 
-import { ContentContainer, ContentInput, HeaderContainer, HeaderLogoTypo, Root } from './styled'
 import * as XLSX from 'xlsx/xlsx.mjs'
+import { ContentContainer, ContentInput, HeaderContainer, HeaderLogoTypo, Root } from './styled'
 
 type LectureExcelPageProps = {
   className?: string
@@ -26,54 +26,10 @@ export const LectureExcelPage: FC<LectureExcelPageProps> = ({ className }) => {
       const ws = wb.Sheets[wsname]
       const data = XLSX.utils.sheet_to_json(ws, { header: 1 })
       e.target.value = null
-      setExcelData((prev: any) => {
-        // let prevPhoneNumberList = prev.filter((value: any) => value[4]?.value).map((value: any) => value[4].value)
 
-        let newData = [
-          ...prev,
-          ...data
-            // .filter((value: any) => !prevPhoneNumberList.includes(value[4]))
-            .filter((value: any) => value[0])
-            .filter((_value: any, index: number) => index !== 0)
-            .map((value: string[]) => value.map((value2) => ({ value: value2 }))),
-        ]
-
-        newData = newData.map((value: any) => {
-          if (!value[5]) {
-            return value
-          }
-          let a = newData.filter((value2: any) => value2[5].value === value[5].value)
-          if (a.length > 1) {
-            a = a[0]
-            console.log({ value, a })
-            for (let i = 0; i < 13; i++) {
-              if (i === 3 || i === 4 || i === 5 || i === 6 || i === 7) {
-                continue
-              }
-              if (a[i]?.value && value[i]?.value && a[i]?.value !== value[i]?.value) {
-                value[i] = { value: `${a[i].value}, ${value[i].value}` }
-              } else if (value[i]?.value) {
-                value[i] = { value: `${value[i].value}` }
-              } else if (a[i]?.value) {
-                value[i] = { value: `${a[i].value}` }
-              }
-            }
-          }
-
-          return [...value]
-        })
-
-        newData = newData.filter((value: any, index: number) => {
-          let check = false
-          newData.forEach((value2: any, index2: number) => {
-            if (value2[4]?.value && value[4]?.value && value2[4].value === value[4].value && index !== index2) {
-              if (index < index2) {
-                check = true
-              }
-            }
-          })
-          return !check
-        })
+      console.log({ data })
+      setExcelData(() => {
+        let newData = data
 
         return [...newData]
       })
