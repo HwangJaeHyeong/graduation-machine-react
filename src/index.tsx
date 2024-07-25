f3839d184582d832f6a49418f635a344b53cc6da
DGUHJH <ghkdwogud852@gmail.com>
2024-07-25 20:26:11 +0900

    feat : MainPage 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import 'antd/dist/reset.css'
-import { ConditionEditPage } from 'pages/Legacy/Condition/Edit'
-import { GraduationDecisionPage } from 'pages/Legacy/Graduation/Decision'
-import { LectureExcelPage } from 'pages/Legacy/Lecture/Excel'
-import { LectureGroupPage } from 'pages/Legacy/Lecture/Group'
-import { MainPage } from 'pages/Legacy/Main'
+import { ConditionEditPage as LegacyConditionEditPage } from 'pages/Legacy/Condition/Edit'
+import { GraduationDecisionPage as LegacyGraduationDecisionPage } from 'pages/Legacy/Graduation/Decision'
+import { LectureExcelPage as LegacyLectureExcelPage } from 'pages/Legacy/Lecture/Excel'
+import { LectureGroupPage as LegacyLectureGroupPage } from 'pages/Legacy/Lecture/Group'
+import { MainPage as LegacyMainPage } from 'pages/Legacy/Main'
+import { MainPage } from 'pages/Main'
 import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -23,11 +24,12 @@ root.render(
     <VersionCheckProvider />
     <BrowserRouter>
       <Routes>
-        <Route path="/legacy/condition/edit/:major_code/:year" element={<ConditionEditPage />} />
-        <Route path="/legacy/lecture/excel" element={<LectureExcelPage />} />
-        <Route path="/legacy/lecture/group" element={<LectureGroupPage />} />
-        <Route path="/legacy/graduation/decision/:major_code/:year" element={<GraduationDecisionPage />} />
-        <Route path="/legacy/" element={<MainPage />} />
+        <Route path="/legacy/condition/edit/:major_code/:year" element={<LegacyConditionEditPage />} />
+        <Route path="/legacy/lecture/excel" element={<LegacyLectureExcelPage />} />
+        <Route path="/legacy/lecture/group" element={<LegacyLectureGroupPage />} />
+        <Route path="/legacy/graduation/decision/:major_code/:year" element={<LegacyGraduationDecisionPage />} />
+        <Route path="/legacy/" element={<LegacyMainPage />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
