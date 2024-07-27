0e91f6e10a319cbddd07efebe02a9b4c7ffd8aa1
DGUHJH <ghkdwogud852@gmail.com>
2024-07-27 21:36:30 +0900

    style : MainPage layout

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,10 +1,50 @@
+import { Header } from 'components/Header'
+import banner01Img from 'constants/images/banner_01.png'
+import logo01Img from 'constants/images/logo_01.png'
 import { FC } from 'react'
-import { Root } from './styled'
+import { useNavigate } from 'react-router-dom'
+import {
+  BannerImg,
+  ButtonContainer,
+  ContentContainer,
+  LogoImg,
+  LogoImgWrapper,
+  Root,
+  SubmitButton,
+  SubtitleTypo,
+  TitleContainer,
+  TitleTypo,
+} from './styled'
 
 type MainPageProps = {
   className?: string
 }
 
 export const MainPage: FC<MainPageProps> = ({ className }) => {
-  return <Root className={className}>MainPage</Root>
+  const navigate = useNavigate()
+
+  const onClickSubmitButton = () => {
+    navigate('/submit')
+  }
+
+  return (
+    <Root className={className}>
+      <Header />
+      <BannerImg src={banner01Img} alt={'배너 이미지'} />
+      <ContentContainer>
+        <LogoImgWrapper>
+          <LogoImg src={logo01Img} alt={'동국대학교 로고 이미지'} />
+        </LogoImgWrapper>
+        <TitleContainer>
+          <TitleTypo>동국대학교 컴퓨터 공학전공 졸업요건 검사 사이트</TitleTypo>
+          <SubtitleTypo>DGU Graduation Machine</SubtitleTypo>
+        </TitleContainer>
+        <ButtonContainer>
+          <SubmitButton type={'primary'} onClick={onClickSubmitButton}>
+            검사하기
+          </SubmitButton>
+        </ButtonContainer>
+      </ContentContainer>
+    </Root>
+  )
 }
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,3 +1,76 @@
+import { Button, Typography } from 'antd'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`
+
+export const BannerImg = styled.img`
+  width: 1920px;
+  z-index: 0;
+`
+
+export const ContentContainer = styled.div`
+  width: 640px;
+  height: 722px;
+  background: #fffb;
+  padding: 50px;
+  box-sizing: border-box;
+  border-radius: 64px;
+  position: absolute;
+  top: 135px;
+  left: 50%;
+  transform: translate(-50%, 0);
+`
+
+export const LogoImgWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+export const LogoImg = styled.img`
+  width: 130px;
+  height: 33px;
+  object-fit: contain;
+`
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 55px;
+`
+
+export const TitleTypo = styled(Typography)`
+  font-size: 24px;
+  font-weight: bold;
+  color: #595959;
+`
+
+export const SubtitleTypo = styled(Typography)`
+  font-size: 40px;
+  font-weight: bold;
+  color: #595959;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 120px;
+`
+
+export const SubmitButton = styled(Button)`
+  width: 250px;
+  height: 66px;
+  font-size: 28px;
+  font-weight: bold;
+  border-radius: 12px;
+`
