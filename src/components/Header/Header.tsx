332c8bac635b44d84764450f437aafb7a2931c84
DGUHJH <ghkdwogud852@gmail.com>
2024-07-29 15:42:14 +0900

    feat : Header onClick event 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import logo02Img from 'constants/images/logo_02.png'
 import { FC } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { LogoImg, MenuContainer, MenuTypo, Root } from './styled'
 
 type HeaderProps = {
@@ -10,15 +10,40 @@ type HeaderProps = {
 
 export const Header: FC<HeaderProps> = ({ className, type = 'USER' }) => {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const onClickMenu = (type: 'CONDITION' | 'TIMETABLE' | 'COMMON_LECTURE_GROUP') => () => {
+    if (type === 'CONDITION') {
+      navigate('/admin/condition')
+      return
+    }
+    if (type === 'TIMETABLE') {
+      navigate('/admin/timetable')
+      return
+    }
+    if (type === 'COMMON_LECTURE_GROUP') {
+      navigate('/admin/common-lecture-group')
+      return
+    }
+  }
 
   return (
     <Root className={className}>
       <LogoImg alt={'로고 이미지'} src={logo02Img} />
       {type === 'ADMIN' && (
         <MenuContainer>
-          <MenuTypo isPointed={pathname.indexOf('condition') !== -1}>졸업 이수 조건 수정</MenuTypo>
-          <MenuTypo isPointed={pathname.indexOf('timetable') !== -1}>전체 시간표 입력</MenuTypo>
-          <MenuTypo isPointed={pathname.indexOf('common-lecture-group') !== -1}>공통 강의 그룹 수정</MenuTypo>
+          <MenuTypo isPointed={pathname.indexOf('condition') !== -1} onClick={onClickMenu('CONDITION')}>
+            졸업 이수 조건 수정
+          </MenuTypo>
+          <MenuTypo isPointed={pathname.indexOf('timetable') !== -1} onClick={onClickMenu('TIMETABLE')}>
+            전체 시간표 입력
+          </MenuTypo>
+          <MenuTypo
+            isPointed={pathname.indexOf('common-lecture-group') !== -1}
+            onClick={onClickMenu('COMMON_LECTURE_GROUP')}
+          >
+            공통 강의 그룹 수정
+          </MenuTypo>
         </MenuContainer>
       )}
     </Root>
