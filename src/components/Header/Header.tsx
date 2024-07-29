921d766467d3d3d22350002e7fc5197700ababa9
DGUHJH <ghkdwogud852@gmail.com>
2024-07-29 15:40:03 +0900

    feat : Header component admin type 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,26 @@
 import logo02Img from 'constants/images/logo_02.png'
 import { FC } from 'react'
-import { LogoImg, Root } from './styled'
+import { useLocation } from 'react-router-dom'
+import { LogoImg, MenuContainer, MenuTypo, Root } from './styled'
 
 type HeaderProps = {
   className?: string
+  type?: 'ADMIN' | 'USER'
 }
 
-export const Header: FC<HeaderProps> = ({ className }) => {
+export const Header: FC<HeaderProps> = ({ className, type = 'USER' }) => {
+  const { pathname } = useLocation()
+
   return (
     <Root className={className}>
       <LogoImg alt={'로고 이미지'} src={logo02Img} />
+      {type === 'ADMIN' && (
+        <MenuContainer>
+          <MenuTypo isPointed={pathname.indexOf('condition') !== -1}>졸업 이수 조건 수정</MenuTypo>
+          <MenuTypo isPointed={pathname.indexOf('timetable') !== -1}>전체 시간표 입력</MenuTypo>
+          <MenuTypo isPointed={pathname.indexOf('common-lecture-group') !== -1}>공통 강의 그룹 수정</MenuTypo>
+        </MenuContainer>
+      )}
     </Root>
   )
 }
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,3 +1,4 @@
+import { Typography } from 'antd'
 import styled from 'styled-components'
 
 export const Root = styled.div`
@@ -5,7 +6,6 @@ export const Root = styled.div`
   height: 95px;
   background: #ea5514f7;
   display: flex;
-  justify-content: space-between;
   align-items: center;
   padding: 0 40px;
   box-sizing: border-box;
@@ -16,4 +16,18 @@ export const LogoImg = styled.img`
   width: 330px;
   height: 56px;
   object-fit: contain;
+`
+
+export const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-left: 20px;
+`
+
+export const MenuTypo = styled(Typography)<{ isPointed?: boolean }>`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) => (props.isPointed ? `#fff` : `#fffa`)};
+  cursor: pointer;
 `
