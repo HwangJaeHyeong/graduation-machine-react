250e0f136e7b1e35c450e0e1c2f29f7151281d8d
DGUHJH <ghkdwogud852@gmail.com>
2024-07-30 21:17:07 +0900

    feat : PreLectureGroupAddModal 레이아웃 수정

--- src/pages/Admin/Condition/Detail/components/PreLectureGroupAddModal/PreLectureGroupAddModal.tsx
+++ src/pages/Admin/Condition/Detail/components/PreLectureGroupAddModal/PreLectureGroupAddModal.tsx
@@ -1,10 +1,26 @@
-import { FC } from 'react'
-import { Root } from './styled'
+import { FC, useEffect, useState } from 'react'
+import { ContentAddButtonIcon, ContentButton, ContentModal, Root } from './styled'
 
 type PreLectureGroupAddModalProps = {
   className?: string
 }
 
 export const PreLectureGroupAddModal: FC<PreLectureGroupAddModalProps> = ({ className }) => {
-  return <Root className={className}>PreLectureGroupAddModal</Root>
+  const [opened, setOpened] = useState<boolean>(false)
+
+  const onClickButton = () => {
+    setOpened(true)
+  }
+
+  useEffect(() => {}, [])
+
+  return (
+    <Root className={className}>
+      <ContentButton onClick={onClickButton}>
+        선이수 강의 추가
+        <ContentAddButtonIcon />
+      </ContentButton>
+      <ContentModal open={opened} onCancel={() => setOpened(false)}></ContentModal>
+    </Root>
+  )
 }
--- src/pages/Admin/Condition/Detail/components/PreLectureGroupAddModal/styled.ts
+++ src/pages/Admin/Condition/Detail/components/PreLectureGroupAddModal/styled.ts
@@ -1,3 +1,15 @@
+import { PlusCircleOutlined } from '@ant-design/icons'
+import { Button, Modal } from 'antd'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+`
+
+export const ContentButton = styled(Button)``
+
+export const ContentModal = styled(Modal)``
+
+export const ContentAddButtonIcon = styled(PlusCircleOutlined)`
+  font-size: 16px;
+`
