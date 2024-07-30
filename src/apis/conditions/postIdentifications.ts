3888c6177f43592e77df378c9d803a711deb4331
DGUHJH <ghkdwogud852@gmail.com>
2024-07-30 21:36:26 +0900

    feat : postIdentifications api 추가

---
+++ src/apis/conditions/postIdentifications.ts
@@ -0,0 +1,21 @@
+import { commonAxios, CommonResponseType } from 'apis/common'
+import { camelizeKey } from 'utils/camelizeKey'
+
+type Props = {
+  groupId: number
+  type: 'none' | 'name' | 'code'
+  keyword?: string
+}
+
+type Type = {}
+
+export const postIdentifications = async ({ groupId, type: defaultType, keyword }: Props) => {
+  const type = defaultType ?? 'none'
+
+  return await commonAxios('POST')(`api/v1/lecture-groups/${groupId}/lectures?type=${type}`, {
+    keyword,
+  }).then((res) => {
+    const response = camelizeKey(res?.data) as CommonResponseType<Type>
+    return response
+  })
+}
