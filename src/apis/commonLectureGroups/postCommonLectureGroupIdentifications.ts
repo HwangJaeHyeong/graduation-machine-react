6e9b5eb63da0a856e1c953ca0c0423df234d6370
DGUHJH <ghkdwogud852@gmail.com>
2024-07-31 18:11:28 +0900

    feat : postCommonLectureGroupIdentification api 추가

---
+++ src/apis/commonLectureGroups/postCommonLectureGroupIdentifications.ts
@@ -0,0 +1,20 @@
+import { commonAxios, CommonResponseType } from 'apis/common'
+import { camelizeKey } from 'utils/camelizeKey'
+
+type Props = {
+  commonLectureGroupId: number
+  type: 'none' | 'name' | 'code'
+  keyword?: string | number
+}
+
+type Type = {}
+
+export const postCommonLectureGroupIdentifications = async ({ commonLectureGroupId, type, keyword }: Props) => {
+  return await commonAxios('POST')(`api/v1/common-lecture-groups/lectures/${commonLectureGroupId}`, {
+    type,
+    keyword,
+  }).then((res) => {
+    const response = camelizeKey(res?.data) as CommonResponseType<Type>
+    return response
+  })
+}
