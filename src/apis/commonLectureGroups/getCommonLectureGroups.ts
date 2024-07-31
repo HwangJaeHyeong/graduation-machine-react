336d61730df39d83dc0f560662e36f9e3e73e390
DGUHJH <ghkdwogud852@gmail.com>
2024-07-31 18:25:00 +0900

    fix : getCommonLectureGroups props 제거

--- src/apis/commonLectureGroups/getCommonLectureGroups.ts
+++ src/apis/commonLectureGroups/getCommonLectureGroups.ts
@@ -8,7 +8,7 @@ type Type = {
   name: string
 }[]
 
-export const getCommonLectureGroups = async ({}: Props) => {
+export const getCommonLectureGroups = async () => {
   return await commonAxios('GET')(`api/v1/common-lecture-groups`, {}).then((res) => {
     const response = camelizeKey(res?.data) as CommonResponseType<Type>
     return response
