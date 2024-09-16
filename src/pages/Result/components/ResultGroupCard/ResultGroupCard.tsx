de39685d7e452ef667235de686be500b06fb7ad8
DGUHJH <ghkdwogud852@gmail.com>
2024-09-16 16:46:00 +0900

    feat : ResultPage 수강하지 않은 LectureIdentification 빈 정보 제거

--- src/pages/Result/components/ResultGroupCard/ResultGroupCard.tsx
+++ src/pages/Result/components/ResultGroupCard/ResultGroupCard.tsx
@@ -10,8 +10,6 @@ export const ResultGroupCard: FC<ResultGroupCardProps> = ({
   className,
   id,
   name,
-  year,
-  season,
   isPassed,
   isEssential,
   lectureIdentificationItem,
@@ -22,11 +20,15 @@ export const ResultGroupCard: FC<ResultGroupCardProps> = ({
         <CardCollapsePanel header={<CardTitleTypo>{`${name}`}</CardTitleTypo>} key={`lecture_group_card_${id}`}>
           <ContentContainer>
             <ContentTypo>선이수 통과 여부 : {isPassed ? 'P' : 'F'}</ContentTypo>
-            <ContentTypo>
-              이수 학기 : {lectureIdentificationItem.year}-{lectureIdentificationItem.season}
-            </ContentTypo>
-            <ContentTypo>학수 번호 : {lectureIdentificationItem.code}</ContentTypo>
-            <ContentTypo>수강 학점 : {lectureIdentificationItem.credit}학점</ContentTypo>
+            {Object.values(lectureIdentificationItem).length > 0 && (
+              <>
+                <ContentTypo>
+                  이수 학기 : {lectureIdentificationItem.year}-{lectureIdentificationItem.season}
+                </ContentTypo>
+                <ContentTypo>학수 번호 : {lectureIdentificationItem.code}</ContentTypo>
+                <ContentTypo>수강 학점 : {lectureIdentificationItem.credit}학점</ContentTypo>
+              </>
+            )}
             <ContentTypo>필수 여부 : {isEssential ? '필수' : '필수 아님'}</ContentTypo>
           </ContentContainer>
         </CardCollapsePanel>
